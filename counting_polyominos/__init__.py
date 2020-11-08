64bde9574c4a21e6639abc23ed8e50f23096b5e0
Lihi <lihipana@gmail.com>
2020-11-08 23:39:09 +0200

    upload files

---
+++ counting_polyominos/__init__.py
@@ -0,0 +1,83 @@
+import random
+
+
+def add_neighbors(item, untried_set, occupied_set):
+    old_points = untried_set | occupied_set
+    neighbors_set = set()
+
+    if add_left(item, old_points):
+        neighbors_set = neighbors_set | add_left(item, old_points)
+    if add_right(item, old_points):
+        neighbors_set = neighbors_set | add_right(item, old_points)
+    if add_up(item, old_points):
+        neighbors_set = neighbors_set | add_up(item, old_points)
+    if add_down(item, old_points):
+        neighbors_set = neighbors_set | add_down(item, old_points)
+    return neighbors_set
+
+
+def add_left(item, old_points):
+    if (item[0] > 0 and item[1] >= 0) or (item[0] <= 0 and item[1] >=1):
+        new_item = (item[0]-1, item[1])
+        if new_item not in old_points:
+            return {new_item}
+
+
+def add_right(item, old_points):
+    if (item[0] >= 0 and item[1] >= 0) or (item[0] <= 0 and item[1] >= 1):
+        new_item = (item[0] + 1, item[1])
+        if new_item not in old_points:
+            return {new_item}
+
+
+def add_up(item, old_points):
+    new_item = (item[0], item[1]+1)
+    if new_item not in old_points:
+        return {new_item}
+
+
+def add_down(item, old_points):
+    if (item[0] >= 0 and item[1] > 0) or (item[0] <= 0 and item[1] > 1):
+        new_item = (item[0], item[1]-1)
+        if new_item not in old_points:
+            return {new_item}
+
+
+def remove_neighbors(untried_set, new_neighbors):
+    for new_neighbor in new_neighbors:
+        untried_set.remove(new_neighbor)
+    return untried_set
+
+
+def counting_poly(untried_set, occupied_set, polyomino, n):
+    count = 0
+    while len(untried_set) != 0:
+        random_item = random.choice(tuple(untried_set))
+        untried_set.remove(random_item)
+        polyomino.append(random_item)
+        occupied_set = occupied_set | {random_item}
+        count = count + 1
+        if len(polyomino) < n:
+            new_neighbors = add_neighbors(random_item, untried_set, occupied_set)
+            untried_set = untried_set | new_neighbors
+            untried_copy = untried_set.copy()
+            count = count + counting_poly(untried_copy, occupied_set, polyomino, n)
+            untried_set = remove_neighbors(untried_set, new_neighbors)
+        polyomino.pop()
+    occupied_set.clear()
+    return count
+
+
+def main():
+    # untried_set = {(0, 0)}
+    n = int(input("Enter the max size of the polyomino "))
+    # polyomino = []
+    # occupied_set = set()
+    print("fixed(n) for n up to specified limit " + str(n))
+    print(counting_poly({(0, 0)}, set(), [], n))
+
+
+if __name__ == '__main__':
+  main()
+
+
---
+++ test.py
@@ -0,0 +1,56 @@
+import unittest
+from counting_polyominos import counting_poly
+
+
+class TestCounting(unittest.TestCase):
+    def test_sum(self):
+        """
+        test that it can calculate sum of polyominos from number x
+        """
+        up_to_1 = counting_poly({(0, 0)}, set(), [], 1)
+        up_to_2 = counting_poly({(0, 0)}, set(), [], 2)
+        up_to_3 = counting_poly({(0, 0)}, set(), [], 3)
+        up_to_4 = counting_poly({(0, 0)}, set(), [], 4)
+        up_to_5 = counting_poly({(0, 0)}, set(), [], 5)
+        up_to_6 = counting_poly({(0, 0)}, set(), [], 6)
+        up_to_7 = counting_poly({(0, 0)}, set(), [], 7)
+        up_to_8 = counting_poly({(0, 0)}, set(), [], 8)
+        up_to_9 = counting_poly({(0, 0)}, set(), [], 9)
+        up_to_10 = counting_poly({(0, 0)}, set(), [], 10)
+        up_to_11 = counting_poly({(0, 0)}, set(), [], 11)
+        up_to_12 = counting_poly({(0, 0)}, set(), [], 12)
+        up_to_13 = counting_poly({(0, 0)}, set(), [], 13)
+        up_to_14 = counting_poly({(0, 0)}, set(), [], 14)
+
+        fixed_2 = up_to_2 - up_to_1
+        fixed_3 = up_to_3 - up_to_2
+        fixed_4 = up_to_4 - up_to_3
+        fixed_5 = up_to_5 - up_to_4
+        fixed_6 = up_to_6 - up_to_5
+        fixed_7 = up_to_7 - up_to_6
+        fixed_8 = up_to_8 - up_to_7
+        fixed_9 = up_to_9 - up_to_8
+        fixed_10 = up_to_10 - up_to_9
+        fixed_11 = up_to_11 - up_to_10
+        fixed_12 = up_to_12 - up_to_11
+        fixed_13 = up_to_13 - up_to_12
+        fixed_14 = up_to_14 - up_to_13
+
+        self.assertEqual(fixed_2, 2, "should be 2")
+        self.assertEqual(fixed_3, 6, "should be 6")
+        self.assertEqual(fixed_4, 19, "should be 19")
+        self.assertEqual(fixed_5, 63, "should be 63")
+        self.assertEqual(fixed_6, 216, "should be 216")
+        self.assertEqual(fixed_7, 760, "should be 760")
+        self.assertEqual(fixed_8, 2725, "should be 2725")
+        self.assertEqual(fixed_9, 9910, "should be 9910")
+        self.assertEqual(fixed_10, 36446, "should be 36446")
+        self.assertEqual(fixed_11, 135268, "should be 135268")
+        self.assertEqual(fixed_12, 505861, "should be 505861")
+        self.assertEqual(fixed_13, 1903890, "should be 1903890")
+        self.assertEqual(fixed_14, 7204874, "should be 7204874")
+
+
+if __name__ == '__main__':
+    unittest.main()
+    print("Everything passed")
